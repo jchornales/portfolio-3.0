bec97d76a8c33b9c2d93184edbbd05fcaf492c33
Justine Hornales <jchornales.dev@gmail.com>
2026-04-27 13:09:22 +0800

    Added floating menu and skills

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://justinehornales.dev',
   server: { host: true, port: 4321 },
+  integrations: [icon()],
 });
