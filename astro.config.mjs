270a6208f1f63c2242477d588ed8c0a37d76740f
Justine Hornales <jchornales.dev@gmail.com>
2026-05-12 07:21:08 +0800

    chore: changed website url to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://justinehornales.dev",
+  site: "https://justinehornales.github.io",
   server: { host: true, port: 4321 },
   integrations: [icon(), react()],
 });
